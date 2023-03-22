719f6144fe1bbf2eb0224a2c625889faaf35fa55
Indalecio <riosindalecio@hotmail.com>
2023-03-21 22:22:54 -0600

    comentarios

--- scripts/semaforo.py
+++ scripts/semaforo.py
@@ -13,19 +13,19 @@ from time import sleep
 bridge = CvBridge()
 
 def lines(image):
-  gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #escala de grises
-  blur = cv2.medianBlur(gray,5) #filtro para reduccion de ruido
-  _,thresh = cv2.threshold(blur,110,255,cv2.THRESH_BINARY) #umbral de binarizacion
-  canny = cv2.Canny(thresh,60,70,apertureSize=3) #
-  linesP = cv2.HoughLinesP(canny,rho=1, theta=np.pi/180,threshold=20,minLineLength=50,maxLineGap=400)
+  gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)    #escala de grises
+  blur = cv2.medianBlur(gray,5)   #filtro para reduccion de ruido
+  _,thresh = cv2.threshold(blur,110,255,cv2.THRESH_BINARY)    #umbral de binarizacion
+  canny = cv2.Canny(thresh,60,70,apertureSize=3)    #detector de bordes
+  linesP = cv2.HoughLinesP(canny,rho=1, theta=np.pi/180,threshold=20,minLineLength=50,maxLineGap=400)   #detector de lineas
   return linesP
  
 def signal_stop(image):
-  signal = image[250:450,1050:]
-  gray = cv2.cvtColor(signal, cv2.COLOR_BGR2GRAY) #escala de grises
-  blur = cv2.medianBlur(gray,5) #filtro para reduccion de ruido
-  _,thresh_signal = cv2.threshold(blur,125,255,cv2.THRESH_BINARY)
-  contours,_ = cv2.findContours(thresh_signal, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+  signal = image[250:450,1050:]   #recorte de imagen para semaforo
+  gray = cv2.cvtColor(signal, cv2.COLOR_BGR2GRAY)   #escala de grises
+  blur = cv2.medianBlur(gray,5)   #filtro para reduccion de ruido
+  _,thresh_signal = cv2.threshold(blur,125,255,cv2.THRESH_BINARY)   #umbral de binarizacion
+  contours,_ = cv2.findContours(thresh_signal, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)    #deteccion de contornos
   var = ()
   sw = 0
   if contours != var:
@@ -44,54 +44,52 @@ def signal_stop(image):
   return sw
 
 def callback(img):
-  dir = rospy.Publisher('/steering', Float64,queue_size = 10) #publicador de direccion
-  vel = rospy.Publisher('/speed', Float64,queue_size = 10) #publicador de velocidad
+  dir = rospy.Publisher('/steering', Float64,queue_size = 10)   #publicador de direccion
+  vel = rospy.Publisher('/speed', Float64,queue_size = 10)    #publicador de velocidad
   
-  #cv.image = bridge.cv2_to_imgmsg(img encoding="passthrough")    #carro fisico
-  cv_image = bridge.imgmsg_to_cv2(img, "rgb8")                 #simulador
+  #cv.image = bridge.cv2_to_imgmsg(img encoding="passthrough")    #imagen para carro fisico
+  cv_image = bridge.imgmsg_to_cv2(img, "rgb8")    #imagen para simulador
   sw = signal_stop(cv_image)
 
-  image = cv_image[550:900,60:] #recorte de imagen
-  drawlines = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-  linesP = lines(image) #obtencion de lineas
+  image = cv_image[550:900,60:]   #recorte de imagen
+  drawlines = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #para dibujar las lineas
+  linesP = lines(image)   #se llama la funcion de obtencion de lineas
 
-  linesL=[]#para guardar todas las lineas de la izquierda detectadas
-  linesR=[]#para guardar todas las lineas de la derecha detectadas
+  linesL=[]   #para guardar todas las lineas de la izquierda detectadas
+  linesR=[]   #para guardar todas las lineas de la derecha detectadas
   
-  if linesP is not None: #si existe alguna linea entra en la condicion 
-    #Diferencia entre lineas izquierdas y derechas
+  if linesP is not None:    #si existe alguna linea entra en la condicion 
     for i in range(0, len(linesP)):
-      l = linesP[i][0]#selecciona una linea a la vez
-      m = (l[3]-l[1])/(l[2]-l[0])#pendiente de las lineas
+      l = linesP[i][0]    #selecciona una linea a la vez
+      m = (l[3]-l[1])/(l[2]-l[0])   #pendiente de las lineas
       
-      if(m!=0):#filtrar lineas completamente verticales
-        if(m>0):#si es positiva es linea izquierda
-          linesR.append(linesP[i][0])#agrega la linea a una lista
-          
-        else:#si es negativa es linea derecha
-          linesL.append(linesP[i][0])#agrega la linea a una lista
+      if(m!=0):   #filtrar lineas completamente verticales
+        if(m>0):    #si es positiva es linea izquierda
+          linesR.append(linesP[i][0])   #agrega la linea a una lista
+        else:   #si es negativa es linea derecha
+          linesL.append(linesP[i][0])   #agrega la linea a una lista
 
     var=[]
     
-    if linesL != var:#si existe mas de 0 lineas se promedian
+    if linesL != var:   #si existe mas de 0 lineas se promedian
       LL=np.mean(linesL,axis=0)
-    else:#si no la linea es igual a 0,0,0,0
+    else:   #si no la linea es igual a 0,0,0,0
       LL=[0,0,0,0]
     
-    if linesR != var:#si existe mas de 0 lineas se promedian
+    if linesR != var:   #si existe mas de 0 lineas se promedian
       LR=np.mean(linesR,axis=0)
-    else:#si no la linea es igual a 0,0,0,0
+    else:   #si no la linea es igual a 0,0,0,0
       LR=[0,0,0,0]
     
-    if(int(LL[2])-int(LL[0])!=0):#para evitar dividir entre 0 en la pendiente
+    if(int(LL[2])-int(LL[0])!=0):   #para evitar dividir entre 0 en la pendiente
       mL = (LL[3]-LL[1])/(LL[2]-LL[0])
-      if(mL<-0.1):#filtro para pendientes muy horizontales
+      if(mL<-0.1):    #filtro para pendientes muy horizontales
         bL = (LL[1])-(mL*(LL[0]))
         x1_L = int((349-bL)/mL)
         y1_L = 349
         x2_L = int((0-bL)/mL)
         y2_L = 0
-      else:#si es horizontal es 0
+      else:   #si es muy cercano a una linea horizontal es 0
         x1_L = 0
         y1_L = 0
         x2_L = 0
@@ -116,15 +114,15 @@ def callback(img):
       x2_R = 0
       y2_R = 0
     
-    gir = 0.00001557*(x1_L) - 0.0002931*(y1_L) - 0.00001961*(x2_L)  - 0.00008441*(x1_R)  - 0.00005383*(x2_R) + 0.2925 
+    gir = 0.00001557*(x1_L) - 0.0002931*(y1_L) - 0.00001961*(x2_L)  - 0.00008441*(x1_R)  - 0.00005383*(x2_R) + 0.2925 # giro del carro obtenido de la regresion lineal
 
-    if(sw==0):
+    if(sw==0):  #switch para parar el carro
       if(gir>-1.1 and gir<1.1):
         if(gir>-0.22):
           dir.publish(gir-0.04)
           vel.publish(40)
           print("go")
-        if(gir>0.16):
+        if(gir>0.16): #condicion para reducir la velocidad en curbas cerradas
           dir.publish(gir)
           print("bajo velocidad")
           vel.publish(5)
