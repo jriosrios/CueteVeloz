c6d8444ffb2eca671e8f500888458191a3806f4d
jriosrios <riosindalecio@hotmail.com>
2023-04-01 22:28:09 -0600

    actualizacion

--- scripts/autonomus_final.py
+++ scripts/autonomus_final.py
@@ -19,41 +19,11 @@ def lines(image):
   linesP = cv2.HoughLinesP(canny,rho=1, theta=np.pi/180,threshold=20,minLineLength=50,maxLineGap=400)   #detector de lineas
   return linesP
  
-def signal_stop(img):
-  stop_sign = cv2.CascadeClassifier('/home/inda/Desktop/CueteVeloz/scripts/stop_sign/cascade_stop_sign.xml')
-  signal = img[300:450,800:950]
-  gray = cv2.cvtColor(signal, cv2.COLOR_BGR2GRAY)
-  cv2.imshow("semaforo", gray)
-  stop_sign_scaled = stop_sign.detectMultiScale(gray, 1.3, 5)
-  for (x, y, w, h) in stop_sign_scaled:
-    # Draw rectangle around the stop sign
-    stop_sign_rectangle = cv2.rectangle(img, (x,y),
-                                        (x+w, y+h),
-                                        (0, 255, 0), 3)
-    # Write "Stop sign" on the bottom of the rectangle
-    stop_sign_text = cv2.putText(img=stop_sign_rectangle,
-                                    text="Stop Sign",
-                                    org=(x, y+h+30),
-                                    fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                                    fontScale=1, color=(0, 0, 255),
-                                    thickness=2, lineType=cv2.LINE_4)
-  
-  if stop_sign_scaled != ():
-    sw = 1
-  else:
-    sw = 0
-  return sw
 
 def callback(img):
   dir = rospy.Publisher('/steering', Float64,queue_size = 10)   #publicador de direccion
-  vel = rospy.Publisher('/speed', Float64,queue_size = 10)    #publicador de velocidad
   
-  #cv.image = bridge.cv2_to_imgmsg(img encoding="passthrough")    #imagen para carro fisico
   cv_image = bridge.imgmsg_to_cv2(img, "rgb8")
-
-  
-  sw = signal_stop(cv_image)
-
   image = cv_image[550:900,60:]   #recorte de imagen
   drawlines = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #para dibujar las lineas
   linesP = lines(image)   #se llama la funcion de obtencion de lineas
@@ -85,6 +55,7 @@ def callback(img):
     
     if(int(LL[2])-int(LL[0])!=0):   #para evitar dividir entre 0 en la pendiente
       mL = (LL[3]-LL[1])/(LL[2]-LL[0])
+
       if(mL<-0.1):    #filtro para pendientes muy horizontales
         bL = (LL[1])-(mL*(LL[0]))
         #se extienden las lineas hasta los bordes de la imagen
@@ -92,11 +63,13 @@ def callback(img):
         y1_L = 349
         x2_L = int((0-bL)/mL)
         y2_L = 0
+
       else:   #si es muy cercano a una linea horizontal es 0
         x1_L = 0
         y1_L = 0
         x2_L = 0
         y2_L = 0
+
     else:   #si se divide entre 0 es 0
       x1_L = 0
       y1_L = 0
@@ -111,6 +84,7 @@ def callback(img):
       y1_R = 349
       x2_R = int((0-bR)/mR)
       y2_R = 0
+
     else    :#si se divide entre 0 es 0
       x1_R = 0
       y1_R = 0
@@ -122,21 +96,7 @@ def callback(img):
     #                     x1    y1      x2    y2
     
     gir = 0.00001557*(x1_L) - 0.0002931*(y1_L) - 0.00001961*(x2_L)  - 0.00008441*(x1_R)  - 0.00005383*(x2_R) + 0.2925 # giro del carro obtenido de la regresion lineal
-
-    if(sw == 0):  #switch para parar el carro
-      if(gir > -1.1 and gir < 1.1):
-        if(gir > -0.22):
-          dir.publish(gir-0.04)
-          vel.publish(40)
-          print("go")
-        if(gir > 0.16): #condicion para reducir la velocidad en curbas cerradas
-          dir.publish(gir)
-          print("bajo velocidad")
-          vel.publish(5)
-    else:
-      vel.publish(0)
-      print("parar")
-      sleep(3)
+    dir.publish(gir-0.04)
   
   cv2.imshow("Image window", drawlines)
   cv2.waitKey(1)
---
+++ scripts/stopsign_final.py
@@ -0,0 +1,71 @@
+import numpy as np
+import roslib
+import sys
+import rospy
+import cv2
+from std_msgs.msg import Float64
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+from time import sleep
+
+bridge = CvBridge()
+
+"""
+def signal_stop(img):
+  stop_sign = cv2.CascadeClassifier('/home/inda/Desktop/CueteVeloz/scripts/cascade_stop_sign.xml')
+  signal = img[300:450,800:950]
+  cv2.imshow("signal", signal)
+  gray = cv2.cvtColor(signal, cv2.COLOR_BGR2GRAY)
+  cv2.imshow("semaforo", gray)
+
+  stop_sign_scaled = stop_sign.detectMultiScale(gray, 1.3, 5)
+  
+  for (x, y, w, h) in stop_sign_scaled:
+    # Draw rectangle around the stop sign
+    stop_sign_rectangle = cv2.rectangle(img, (x,y),
+                                        (x+w, y+h),
+                                        (0, 255, 0), 3)
+    # Write "Stop sign" on the bottom of the rectangle
+    stop_sign_text = cv2.putText(img=stop_sign_rectangle,
+                                    text="Stop Sign",
+                                    org=(x, y+h+30),
+                                    fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                                    fontScale=1, color=(0, 0, 255),
+                                    thickness=2, lineType=cv2.LINE_4)
+  
+  if stop_sign_scaled != ():
+    sw = 1
+  else:
+    sw = 0
+
+  return sw
+"""
+
+def callback_signal(img):
+    vel = rospy.Publisher('/speed', Float64,queue_size = 10)    #publicador de velocidad
+
+    cv_image = bridge.imgmsg_to_cv2(img, "rgb8")
+    cv2.imshow("Image window", cv_image)
+    """
+    sw = signal_stop(cv_image)
+
+    if(sw == 0):  #switch para parar el carro
+        vel.publish(30)
+    else:
+        vel.publish(0)
+        sleep(3)
+
+def main(args):
+  rospy.init_node('image_converter', anonymous=True)
+  image_sub = rospy.Subscriber("/camera/rgb/raw", Image, callback_signal)
+
+  try:
+    rospy.spin()
+
+  except KeyboardInterrupt:
+    print("Shutting down")
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+  main(sys.argv)
+  
